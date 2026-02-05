827dbfc51e7ab60fcaccbf309cc0276d38355c6f
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-02-05 10:43:22 +0100

    start project

---
+++ cartella snack-1/script.js
@@ -0,0 +1,19 @@
+/*
+snack 1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+
+
+Cosa fare:
+Leggi la tracchia che vedi sopra
+Fai una cartella js-snacks-blocco-1
+metti dentro una sotto cartella snack-1
+metti nella sotto cartella due file index.html e main.js
+svolgi l'esercizio
+pusha
+mandami messaggio qui con il link della repo quando hai fatto.
+*/
+
+
+// Snack 1
+// L’utente inserisce due numeri, il software stampa il maggiore
