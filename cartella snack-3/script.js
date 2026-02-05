44cab9bf0580495836e9dbe57640b644a12b9ccc
mariachiaramolinarilavoro-bit <mariachiaramolinari.lavoro@gmail.com>
2026-02-05 12:16:46 +0100

    snack 3

---
+++ cartella snack-3/script.js
@@ -0,0 +1,12 @@
+//cartella: snack-3
+
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
+let sum = 0
+
+for (let i = 0; i < 10; i++){
+    let numbers = prompt ("Inserire un numero");
+    sum += numbers
+}
+
+console.log(sum)
